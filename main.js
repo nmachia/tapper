a25c0c93b3a7eee7aa17c4b06bdd5a6acf2f9c5a
nmachia <nick@abcideabased.com>
2017-01-30 17:13:29 -0500

    Pre Rendering Graphics
    Set up graphics to pre render in attempt to speed up

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ function startAction(e){
 
 //game stuff
 var canvas = document.getElementById('canvas');
+var W = canvas.width = window.innerWidth;
+var H = canvas.height = window.innerHeight;
+var heroSize = Math.floor(W/4);
+var cloudSize = W/9;
 var ctx = canvas.getContext('2d');
 canvas.addEventListener('click', action);
 var clickDisabled = false;
@@ -26,41 +30,61 @@ document.body.style.background = game.backgrounds[0];
 
 var start = null;
 
+//prerendering pig
+var rendered = document.createElement('canvas');
+rendered.width = Math.floor(heroSize * 2);
+rendered.height = Math.floor(heroSize);
 
-var W = canvas.width = window.innerWidth;
-var H = canvas.height = window.innerHeight;
+var renderedCtx = rendered.getContext('2d');
+var pig = new Image();
+pig.onload = function(){
+  renderedCtx.drawImage(pig,0,0, rendered.width, rendered.height);
+};
+pig.src = 'sprite.png';
+
+//prerendering cloud
+var renderedCloud = document.createElement('canvas');
+renderedCloud.width = Math.floor(cloudSize * 4);
+renderedCloud.height = Math.floor(cloudSize*2);
+var renderedCloudCtx = renderedCloud.getContext('2d');
+renderedCloudCtx.beginPath();
+renderedCloudCtx.arc(cloudSize*2, cloudSize, cloudSize, 0, 2 * Math.PI, false);
+renderedCloudCtx.arc(cloudSize, cloudSize, cloudSize/1.3, 0, 2 * Math.PI, false);
+renderedCloudCtx.arc(cloudSize*3, cloudSize, cloudSize/1.4, 0, 2 * Math.PI, false);
+renderedCloudCtx.fillStyle = '#EEEEEE';
+renderedCloudCtx.fill();
+
+
+
 
 function Ball(){
-  this.radius = W / 4;
-  this.x = W / 2;
+  this.radius = heroSize;
+  this.x = Math.floor(W / 2);
   this.y = H - this.radius;
   this.vy = null;
   this.sprite = 0;
   this.spriteSwitch = false;
-  this.imageUp = new Image();
-  this.imageUp.src = 'pigUp.svg';
-  this.imageDown = new Image();
-  this.imageDown.src = 'pig.svg';
   this.minSpeed = 0;
   this.maxSpeed = 2;
   this.vx = Math.random(this.minSpeed, this.maxSpeed);
+
   this.draw = function(ctx) {
-      if(this.vy > 0 && this.sprite % 16 <= 8){
-        ctx.drawImage(this.imageDown, this.x-this.radius/2, this.y, this.radius, this.radius);
-        //console.log('draw1');
+      if(this.vy > 0 && this.spriteSwitch ){
+         ctx.drawImage(rendered,this.radius,0,this.radius,this.radius,this.x,this.y, this.radius, this.radius);
       }else{
+          this.sprite++;
+
          if(this.sprite % 16 === 0){
             this.spriteSwitch ^= true;
           }
+
         if(this.spriteSwitch){
-          ctx.drawImage(this.imageUp, this.x-this.radius/2, this.y, this.radius, this.radius);
-          //console.log('draw2');
+          ctx.drawImage(rendered,this.radius,0,this.radius,this.radius,this.x,this.y, this.radius, this.radius);
           
         }else{
-          ctx.drawImage(this.imageDown, this.x-this.radius/2, this.y, this.radius, this.radius);
-          //console.log('draw3');
+           ctx.drawImage(rendered,0,0,this.radius, this.radius,this.x,this.y, this.radius, this.radius);
         }
-        this.sprite++;
+        
 
       }
     };
@@ -81,10 +105,7 @@ function Clouds(){
       if(rando === 0) {
         this.group.push( {
           x: Math.floor(Math.random()* canvas.width),
-          y: 0,
-          w: canvas.width/3,
-          r: canvas.width/9,
-          h: canvas.height/8,
+          y: 0 - cloudSize*2,
           d: Math.random()
         });
       }
@@ -94,7 +115,7 @@ function Clouds(){
       var item = this.group[i];
       item.y += 1;
       
-      if( item.y - (item.r * 2) > H ) {
+      if( item.y - (cloudSize * 2) > H ) {
         this.group.splice(i, 1);
       }
     }
@@ -102,13 +123,7 @@ function Clouds(){
   this.draw = function(){
     for(var i = 0; i< this.group.length; i++ ) {
       var item = this.group[i];
-
-      ctx.beginPath();
-      ctx.arc(item.x, item.y-item.r, item.r, 0, 2 * Math.PI, false);
-      ctx.arc(item.x-item.r, item.y-item.r, item.r/1.3, 0, 2 * Math.PI, false);
-      ctx.arc(item.x+item.r, item.y-item.r, item.r/1.4, 0, 2 * Math.PI, false);
-      ctx.fillStyle = '#EEEEEE';
-      ctx.fill();
+      ctx.drawImage(renderedCloud,0,0,cloudSize*4, cloudSize*2,item.x,item.y, cloudSize*4, cloudSize*2);
     }
   };
 }
@@ -124,7 +139,7 @@ var resetGame = function(){
   game.level = 1;
   game.gravity = 0.1;
   ball.vy = null;
-  ball.x = canvas.width / 2;
+  ball.x = canvas.width / 2 - ball.radius/2;
   ball.y = canvas.height - ball.radius;
   clouds.group.length = 0;
   ball.draw(ctx);
@@ -184,10 +199,12 @@ function action(e){
             //otherwise its an in-game hit
             game.score ++;
             ball.vy *= -1;
-            if(eventLocation.x >= (ball.x + 0.2 * ball.radius) && ball.vx >= 0){
+            if(eventLocation.x >= (ball.x + 0.65 * ball.radius)){
               ball.vx = -(Math.random(this.maxSpeed/2,this.maxSpeed));
-            }else if(eventLocation.x <= (ball.x - 0.2 * ball.radius) && ball.vx <= 0){
+            }else if(eventLocation.x <= (ball.x + 0.35 * ball.radius)){
               ball.vx = (Math.random(this.maxSpeed/2,this.maxSpeed));
+            }else{
+              ball.vx = (Math.random(-(this.maxSpeed/2),this.maxSpeed/2));
             }
             document.getElementById('score').innerHTML = game.score;
             //transition background every 10 hits (needs work)
@@ -218,10 +235,11 @@ var step = function( timestamp ) {
     ball.x += ball.vx;
     ball.y += ball.vy;
 
+
     //change x direction if you hit a wall
     if (
-        ball.x + ball.radius/2 > canvas.width ||
-        ball.x-ball.radius/2  < 0 ){
+        ball.x + ball.radius > canvas.width ||
+        ball.x  < 0 ){
       ball.vx *= -1;
     }
 
@@ -235,6 +253,8 @@ var step = function( timestamp ) {
 
 
     ball.draw(ctx);
+
+
 
     clouds.create();
     clouds.move();
